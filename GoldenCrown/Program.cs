96751f54f2d0166b147f2b5f580dd33c63444b6c
Art'm <kasko.artem45@gmail.com>
2026-05-22 02:32:29 +0300

    Configure ApplicationDbContext with SQL Server

--- GoldenCrown/Program.cs
+++ GoldenCrown/Program.cs
@@ -1,3 +1,6 @@
+using GoldenCrown.Database;
+using Microsoft.EntityFrameworkCore;
+
 namespace GoldenCrown
 {
     public class Program
@@ -7,7 +10,10 @@ namespace GoldenCrown
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+                                   ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found");
 
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();

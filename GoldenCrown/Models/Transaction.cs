4c2670b5c474aab22ec63b003bff0f9da27c3a01
Art'm <kasko.artem45@gmail.com>
2026-05-22 00:06:57 +0300

    Create Transaction model

---
+++ GoldenCrown/Models/Transaction.cs
@@ -0,0 +1,10 @@
+namespace GoldenCrown.Models;
+
+public class Transaction
+{
+    public int Id { get; set; }
+    public int SenderId { get; set; }
+    public int ReceiverId { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public decimal Amount { get; set; }
+}

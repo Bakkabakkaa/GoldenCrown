d70370b682aef7eb69fdc6d27c82de258009f879
Art'm <kasko.artem45@gmail.com>
2026-05-26 01:35:27 +0300

    Create RegisterRequest DTO

---
+++ GoldenCrown/Dtos/RegisterRequest.cs
@@ -0,0 +1,8 @@
+namespace GoldenCrown.Dtos;
+
+public class RegisterRequest
+{
+    public string Login { get; set; }
+    public string Name { get; set; }
+    public string Password { get; set; }
+}

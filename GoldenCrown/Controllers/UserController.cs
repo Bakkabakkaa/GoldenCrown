fcff3ebec84cf7d22a40c96697f44ac0182b9970
Art'm <kasko.artem45@gmail.com>
2026-05-28 23:00:12 +0300

    Add UserController

---
+++ GoldenCrown/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+using GoldenCrown.Dtos;
+using GoldenCrown.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoldenCrown.Controllers;
+
+[Route("api/user")]
+public class UserController : Controller
+{
+    
+}

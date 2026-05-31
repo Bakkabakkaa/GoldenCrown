9cf34a53e563bf7c67511920e93e7d32267cda6d
Art'm <kasko.artem45@gmail.com>
2026-05-31 20:05:49 +0300

    Add GetBalance method

--- GoldenCrown/Services/FinanceService.cs
+++ GoldenCrown/Services/FinanceService.cs
@@ -1,6 +1,30 @@
+using GoldenCrown.Database;
+using GoldenCrown.Dtos.Finance;
+using Microsoft.EntityFrameworkCore;
+
 namespace GoldenCrown.Services;
 
 public class FinanceService : IFinanceService
 {
+    private readonly ApplicationDbContext _context;
+
+    public FinanceService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
     
+    public async Task<Result<decimal>> GetBalance(string token)
+    {
+        var session = await _context.Sessions.FirstOrDefaultAsync(s => s.Token == token);
+        
+        if (session == null)
+        {
+            return Result<decimal>.Failure("User not authorized");
+        }
+
+        var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == session.UserId);
+        var account = await _context.Accounts.FirstOrDefaultAsync(a => a.UserId == user.Id);
+
+        return Result<decimal>.Success(account.Balance);
+    }
 }

c4b1881023471b3b495a3bc3d2530f049a2b9ad5
Art'm <kasko.artem45@gmail.com>
2026-05-31 21:17:43 +0300

    Add deposit method

--- GoldenCrown/Services/FinanceService.cs
+++ GoldenCrown/Services/FinanceService.cs
@@ -27,4 +27,23 @@ public class FinanceService : IFinanceService
 
         return Result<decimal>.Success(account!.Balance);
     }
+
+    public async Task<Result> DepositAsync(string token, decimal amount)
+    {
+        var session = await _context.Sessions.FirstOrDefaultAsync(s => s.Token == token);
+
+        if (session == null)
+        {
+            return Result.Failure("User not authorized");
+        }
+
+        var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == session.UserId);
+        var account = await _context.Accounts.FirstOrDefaultAsync(a => a.UserId == user!.Id);
+
+        account!.Balance += amount;
+        
+        await _context.SaveChangesAsync();
+        return Result.Success();
+
+    }
 }
--- GoldenCrown/Services/IFinanceService.cs
+++ GoldenCrown/Services/IFinanceService.cs
@@ -3,4 +3,5 @@ namespace GoldenCrown.Services;
 public interface IFinanceService
 {
     Task<Result<decimal>> GetBalanceAsync(string token);
+    Task<Result> DepositAsync(string token, decimal amount);
 }

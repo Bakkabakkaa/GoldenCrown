ad454b523709b951cd2f9c36c352ced15fd8a60b
Art'm <kasko.artem45@gmail.com>
2026-05-31 18:29:26 +0300

    Registration in DI

--- GoldenCrown/Program.cs
+++ GoldenCrown/Program.cs
@@ -18,6 +18,7 @@ namespace GoldenCrown
             
             builder.Services.AddScoped<IUserService, UserService>();
             builder.Services.AddScoped<IAccountService, AccountService>();
+            builder.Services.AddScoped<IFinanceService, FinanceService>();
             
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

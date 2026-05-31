5b9a8fc1e75dbc041e893019d0cb927ef218ef1d
Art'm <kasko.artem45@gmail.com>
2026-05-31 18:17:57 +0300

    Add TransferRequest

--- GoldenCrown/Dtos/Finance/TransferRequest.cs
+++ GoldenCrown/Dtos/Finance/TransferRequest.cs
@@ -3,11 +3,14 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace GoldenCrown.Dtos.Finance;
 
-public class DepositRequest
+public class TransferRequest
 {
     [FromQuery]
     [Required(ErrorMessage = "The 'token' field is required")]
     public string Token { get; set; }
+    
+    [Required(ErrorMessage = "The 'ReceiverLogin' field is required")]
+    public string ReceiverLogin { get; set; }
     
     [Range(0.01, double.MaxValue, ErrorMessage = "The amount must be greater than 0")]
     public decimal Amount { get; set; }

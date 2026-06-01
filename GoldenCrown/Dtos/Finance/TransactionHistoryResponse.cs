daae1b881e3b9ddd7d08bf4d706e6088a1d093d1
Art'm <kasko.artem45@gmail.com>
2026-06-02 00:09:38 +0300

    Add TransactionHistoryResponse model

---
+++ GoldenCrown/Dtos/Finance/TransactionHistoryResponse.cs
@@ -0,0 +1,9 @@
+namespace GoldenCrown.Dtos.Finance;
+
+public class TransactionHistoryResponse
+{
+    public string SenderName { get; set; }
+    public string ReceiverName { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime Date { get; set; }
+}

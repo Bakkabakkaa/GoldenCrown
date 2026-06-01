1d8b27e9a5c70d3c315df25f9d5ee5506ae7d219
Art'm <kasko.artem45@gmail.com>
2026-06-02 01:01:15 +0300

    Add to interface

--- GoldenCrown/Services/IFinanceService.cs
+++ GoldenCrown/Services/IFinanceService.cs
@@ -1,3 +1,5 @@
+using GoldenCrown.Dtos.Finance;
+
 namespace GoldenCrown.Services;
 
 public interface IFinanceService
@@ -5,4 +7,6 @@ public interface IFinanceService
     Task<Result<decimal>> GetBalanceAsync(string token);
     Task<Result> DepositAsync(string token, decimal amount);
     Task<Result> TransferAsync(string fromToken, string toLogin, decimal amount);
+    Task<Result<List<TransactionHistoryResponse>>> GetTransactionHistoryAsync(string token,
+        DateTime? dateFrom, DateTime? dateTo, int skip, int take)
 }

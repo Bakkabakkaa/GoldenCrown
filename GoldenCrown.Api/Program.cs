b20034d97c7961840caf05ffce0c003bdf1d5756
Art'm <kasko.artem45@gmail.com>
2026-06-24 01:36:06 +0300

    Clean architecture 2

---
+++ GoldenCrown.Api/Program.cs
@@ -0,0 +1,78 @@
+using FluentValidation;
+using GoldenCrown.Api.BackGroundServices;
+using GoldenCrown.Api.Middlewares;
+using GoldenCrown.Application.Dtos.User;
+using GoldenCrown.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
+
+namespace GoldenCrown
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+                ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            builder.Services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(typeof(Program).Assembly);
+            });
+            builder.Services.AddValidatorsFromAssemblyContaining<LoginRequest>();
+            builder.Services.AddHostedService<SessionCleanupService>();
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    Description = "Введите токен: Bearer your-token"
+                });
+
+                options.AddSecurityRequirement(document => new OpenApiSecurityRequirement
+                {
+                    [new OpenApiSecuritySchemeReference("Bearer", document)] = []
+                });
+            });
+            
+            var app = builder.Build();
+            
+            MigrateDatabase(app);
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseMiddleware<AuthorizationMiddleware>();
+
+            app.MapControllers();
+            
+            app.Run();
+        }
+
+        private static void MigrateDatabase(WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            db.Database.Migrate();
+        }
+    }
+}
